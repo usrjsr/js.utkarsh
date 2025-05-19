0f5a3e5bb12bdc960a028c61f16d1f0d053b02db
Utkarsh Singh <utkarshsinghdau@gmail.com>
2025-05-19 02:47:44 +0000

    let var and const

--- .devcontainer/utkarsh/start.js
+++ .devcontainer/utkarsh/start.js
@@ -1,1 +1,28 @@
-console.log("UTKARSH")
+const accountId =14446
+let accountEmail="utkarshsinghdau@gmail.com"
+var pass="12345"
+accountCity="Ranchi"
+let accountState
+//accountId=3 //not allowed
+//var prefer not to use due to its block and functional scope
+
+
+
+accountEmail="hc@ut.com"
+pass="45678"
+accountCity="Jaipur"
+console.log(accountId)
+console.table([accountState,accountId,accountEmail,pass,accountCity])
+
+// number 2^53
+// bigint for large number
+// string => " "
+// boolean => true/false
+// null=> standone value (can be assign)
+// undefiend => value is not assigned yet
+// symbol => uniqueness
+
+//object
+
+console.log(typeof(undefined)) //undefined
+console.log(typeof(null)) //object

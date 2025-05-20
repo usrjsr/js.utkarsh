e8f3f1edbc3f3ddbe33b9fd7be6f969acf31c286
Utkarsh Singh <utkarshsinghdau@gmail.com>
2025-05-20 10:51:06 +0000

    string

---
+++ .devcontainer/utkarsh/strings.js
@@ -0,0 +1,35 @@
+// const name="Utkarsh "
+// const repocount= 59
+
+// console.log(name+repocount)//outdated
+// console.log(`My name is ${name} & my repocount is ${repocount}`)// this is mostly used these days ``=>backtick
+// let myname={
+//     name: "utkarsh"
+// }
+// let newname=myname
+// newname.name=newname.name.toUpperCase();
+// console.log(newname.name)
+// console.log(myname.name)//reference passed so any changes done on newname will appear also in newname
+
+ const gameName = String("Utkarsh Singh Rajput");
+// console.log(gameName[0]);
+//  console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName);
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('t'))
+// const newString=gameName.substring(0,3)
+// const newString1=gameName.substring(0,3)
+// console.log(newString)//utk
+// console.log(newString1)//utk
+
+// const newq="  Utkarsh Singh"
+// console.log(newq.trim())
+// console.log(newq)
+// console.log(newq.replace("ar","5"))
+// console.log(newq.includes("Ut"))
+
+console.log(newGame.split())
+
+

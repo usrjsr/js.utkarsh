fbf79a0d0beb7cf07ca50af2ab0fe0711b359909
Utkarsh Singh <utkarshsinghdau@gmail.com>
2025-05-25 02:06:52 +0000

    controlflow

---
+++ .devcontainer/utkarsh/controlflow.js
@@ -0,0 +1,34 @@
+//if statement 
+
+// if(condition=>true){
+//     if condition is true then it gets executed
+// }
+
+//everything is same as cpp if else switch nested if ternary
+
+//truthe and false values
+
+//false=>0,-0,BigInt 0n,null,undefined,NaN,""empty string
+//truth- => "0",'false'it's inside string so true,[],{},function(){}
+
+//if we have to check array is empty so array.length===0
+//if we have to check object is empty or not so Object.keys(objectname).length===0
+
+
+//Nullish Coalescing Operator(??):undefined
+// let vall;
+// vall=5??10//5
+// vall=null??15//15
+// //if res is there so val1=res otherwise null will be automatically assigned and program will run according to the null value
+// vall=undefined??15//15
+// vall=null??2??3??4//2
+// //first value gets assigned after null or undefined
+// console.log(vall)
+
+
+
+
+/************ITERATION***************/
+
+
+
